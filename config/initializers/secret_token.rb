41907f846c7cf7be2c40e5c64fd5e48dec382678
Alexey Naumov <alexsnaumov@gmail.com>
2015-02-04 02:38:43 +0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AndmeApp::Application.config.secret_key_base = 'ab4821bddf85f27f9f6ec29eb2d73aaf4b81f0c18edf5070ba4059ed125e971527e24caced2c5c6c479a6bd9386c5e87a3b6231d37ac8c3b6c57148027b5eba7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AndmeApp::Application.config.session_store :cookie_store, key: '_andme_app_session'
